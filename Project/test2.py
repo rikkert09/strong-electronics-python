b3a527408a32d22f4f8e5e4db5bf1b4366b4b1a7
Mark <markvandermolen1998@live.nl>
2017-11-03 09:04:19 +0100

    Update GUI (spinbox+comments)

--- Project/test2.py
+++ Project/test2.py
@@ -34,33 +34,54 @@ class SampleApp(tk.Tk):
         frame = self.frames[page_name]
         frame.tkraise()
 
+''''
+The class Mainpage is the front page of the application
+Its a frame that shows whitch controlunit(s) are connected
+'''
 
 class Mainpage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
+        ''''
+        ALL LABELS FOR MAINPAGE
+        '''
+        # a line of text
         Label1 = tk.Label(self, text="Besturingseenheid")
         Label1.grid(row=1, column=0)
 
+        #a row of white space
+        label2 = tk.Label(self)
+        label2.grid(row=2, column=0, sticky="E")
+
+        # a line of text
         label2 = tk.Label(self, text="Uitgerold:")
-        label2.grid(row=2, column=0)
+        label2.grid(row=3, column=0)
 
+        # a line of text
         label3 = tk.Label(self, text="Sensorwaarde:")
-        label3.grid(row=3, column=0)
+        label3.grid(row=4, column=0)
 
+        ''''
+        ALL BUTTONS FOR MAINPAGE
+        '''
+        # Button to roll up the sunshade
         but1 = ttk.Button(self, text="Oprollen", width=10)
-        but1.grid(row=4, column=0)
+        but1.grid(row=5, column=0)
 
+        # Button to roll out the sunshade
         but2 = ttk.Button(self, text="Uitrollen", width=10)
-        but2.grid(row=5, column=0)
+        but2.grid(row=6, column=0)
 
+        # Button to go to detailed information about a particulair Controlunit
         but3 = ttk.Button(self, text="Overzicht", width=10,
                          command=lambda: controller.show_frame("ControlUnit"))
-        but3.grid(row=6, column=0)
+        but3.grid(row=7, column=0)
 
 
-class Overview(tk.Frame):
+#eruit gecomment omdat dit een deel van een button uit control unit voorstelt geloof ik
+''''class Overview(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
@@ -68,8 +89,13 @@ class Overview(tk.Frame):
         label.pack(side="top", fill="x", pady=10)
         button = tk.Button(self, text="Terug",
                            command=lambda: controller.show_frame("Mainpage"))
-        button.pack()
+        button.pack()'''
 
+''''
+The class ControlUnit is the page that will pop-up if you press the button "overzicht"
+from the class Mainpage. The class ControlUnit is used as a overview to see data of a particulair
+Controlunit in which you can adjust settings about that particulair Controlunit.
+'''
 
 class ControlUnit(tk.Frame):
     def __init__(self, parent, controller):
@@ -90,18 +116,18 @@ class ControlUnit(tk.Frame):
         label4 = tk.Label(self, text="Handmatig op/uitrollen (in cm)")
         label4.grid(row=7, column=0, sticky=tk.W)
 
-        scale1 = tk.Scale(self, from_=0, to=100, length=200, tickinterval=20, orient=tk.HORIZONTAL)
+        scale1 = tk.Scale(self, from_=0, to=100, length=200, tickinterval=25, orient=tk.HORIZONTAL)
         scale1.set(0)
         scale1.grid(row=6, column=0)
 
-        Entry1 = tk.Entry(self)
-        Entry1.grid(row=2, column=0, sticky=tk.W)
+        Spinbox1 = tk.Spinbox(self, from_=0,to=100)
+        Spinbox1.grid(row=2, column=0, sticky=tk.W)
 
-        Entry2 = tk.Entry(self)
-        Entry2.grid(row=4, column=0, sticky=tk.W)
+        Spinbox2 = tk.Spinbox(self, from_=0,to=100)
+        Spinbox2.grid(row=4, column=0, sticky=tk.W)
 
-        Entry3 = tk.Entry(self)
-        Entry3.grid(row=8, column=0, sticky=tk.W)
+        Spinbox3 = tk.Spinbox(self, from_=0,to=100)
+        Spinbox3.grid(row=8, column=0, sticky=tk.W)
 
         but1 = ttk.Button(self, text="Terug", width=10, command=lambda: controller.show_frame("Mainpage"))
         but1.grid(row=9, column=0, sticky=tk.W)
@@ -116,5 +142,5 @@ class ControlUnit(tk.Frame):
 if __name__ == "__main__":
     root = SampleApp()
     root.title("Zeng Ltd Controller")
-    #root.iconbitmap('Z.ico')
+    root.iconbitmap('Z.ico')
     root.mainloop()
