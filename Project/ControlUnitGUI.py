522bc4ce492ec4c8544d0e120b5bb8bbe7f72a97
Rick Lenes <rick@lenes.net>
2017-11-12 13:19:51 +0100

    renaming

--- Project/ControlUnitGUI.py
+++ Project/ControlUnitGUI.py
@@ -9,20 +9,13 @@ from matplotlib.figure import Figure
 from matplotlib import style
 
 matplotlib.use("TkAgg")
+style.use('ggplot')
 
 ''''
 The class ControlUnit is the page that will pop-up if you press the button "overzicht"
 from the class Mainpage. The class ControlUnit is used as a overview to see data of a particulair
 Controlunit in which you can adjust settings about that particular Controlunit.
 '''
-style.use('ggplot')
-fig_Light = Figure(figsize=(10, 4), dpi=75)
-a_Light = fig_Light.add_subplot(111)
-
-
-# a_Light.margins(0)
-
-
 
 
 class ControlUnit(tk.Frame):
@@ -38,144 +31,144 @@ class ControlUnit(tk.Frame):
         self.a_Temp = self.fig_Temp.add_subplot(111)
 
         # generates a frame within the frame with padding set to 10
-        marginframe = ttk.Frame(self, padding=10)
-        marginframe.grid(row=1)
+        margin_frame = ttk.Frame(self, padding=10)
+        margin_frame.grid(row=1)
         # generates a labelframe within frame. padding set to 10
-        borderframe = tk.Frame(marginframe, highlightbackground="grey", highlightthickness=1, padx=10, pady=10)
-        borderframe.grid(row=1)
+        border_frame = tk.Frame(margin_frame, highlightbackground="grey", highlightthickness=1, padx=10, pady=10)
+        border_frame.grid(row=1)
 
         name = "Besturingseenheid"
-        newname = Entry(borderframe)
-        newname.insert(0, name)
+        new_name = Entry(border_frame)
+        new_name.insert(0, name)
 
         # Shows the name of the specific ControlUnit
-        namelabel = tk.Label(borderframe, text=name)
-        namelabel.grid(row=0, column=0, sticky=tk.W)
+        name_label = tk.Label(border_frame, text=name)
+        name_label.grid(row=0, column=0, sticky=tk.W)
 
         # Text for the option to set the minimum roll out value of the sunshade
-        minrolllabel = tk.Label(borderframe, text="Minimale Uitrol (in cm)")
-        minrolllabel.grid(row=1, column=0, sticky=tk.W)
+        minroll_label = tk.Label(border_frame, text="Minimale Uitrol (in cm)")
+        minroll_label.grid(row=1, column=0, sticky=tk.W)
 
         # Text for the option to set the maximum roll out value of the sunshade
-        maxrolllabel = tk.Label(borderframe, text="Maximale Uitrol (in cm)")
-        maxrolllabel.grid(row=3, column=0, sticky=tk.W)
+        maxroll_label = tk.Label(border_frame, text="Maximale Uitrol (in cm)")
+        maxroll_label.grid(row=3, column=0, sticky=tk.W)
 
         # Text for the option to set the trigger value of the sensor
-        sensortriglabel = tk.Label(borderframe, text="Sensor Trigger")
-        sensortriglabel.grid(row=5, column=0, sticky=tk.W)
+        sensor_trig_label = tk.Label(border_frame, text="Sensor Trigger")
+        sensor_trig_label.grid(row=5, column=0, sticky=tk.W)
 
         # Text for the option to set the custom value of the sunshade
-        customrolllabel = tk.Label(borderframe, text="Handmatig op/uitrollen (in cm)")
-        customrolllabel.grid(row=7, column=0, sticky=tk.W)
+        custom_roll_label = tk.Label(border_frame, text="Handmatig op/uitrollen (in cm)")
+        custom_roll_label.grid(row=7, column=0, sticky=tk.W)
 
         # Scale is the slidebar to set the sensor trigger
-        self.sensortrigscale = tk.Scale(borderframe, from_=0, to=100, length=200, tickinterval=25,
-                                        orient=tk.HORIZONTAL)
+        self.sensor_trig_scale = tk.Scale(border_frame, from_=0, to=100, length=200, tickinterval=25,
+                                          orient=tk.HORIZONTAL)
         # sets value
-        self.sensortrigscale.set(25)
-        self.sensortrigscale.grid(row=6, column=0)
+        self.sensor_trig_scale.set(25)
+        self.sensor_trig_scale.grid(row=6, column=0)
 
         ''''
         ALL SPINBOXES FOR CONTROLUNIT
         '''
 
         # Spinbox to set the minimal unrolling position of the sunshade
-        self.minrollspinbox = tk.Spinbox(borderframe, from_=0, to=200, validate='all')
+        self.min_roll_spinbox = tk.Spinbox(border_frame, from_=0, to=200, validate='all')
         # validates if int
-        self.minrollspinbox["validatecommand"] = (self.minrollspinbox.register(self.validate), '%P', '%i', '%d')
-        self.minrollspinbox.delete(0, "end")  # clears value
-        self.minrollspinbox.insert(0, 0)  # sets value
-        self.minrollspinbox.grid(row=2, column=0, sticky=tk.W)
+        self.min_roll_spinbox["validatecommand"] = (self.min_roll_spinbox.register(self.validate), '%P', '%i', '%d')
+        self.min_roll_spinbox.delete(0, "end")  # clears value
+        self.min_roll_spinbox.insert(0, 0)  # sets value
+        self.min_roll_spinbox.grid(row=2, column=0, sticky=tk.W)
 
         # Spinbox to set the maximum unrolling position of the sunshade
-        self.maxrollspinbox = tk.Spinbox(borderframe, from_=0, to=200, validate='all')
+        self.max_roll_spinbox = tk.Spinbox(border_frame, from_=0, to=200, validate='all')
         # validates if int
-        self.maxrollspinbox["validatecommand"] = (self.maxrollspinbox.register(self.validate), '%P', '%i', '%d')
-        self.maxrollspinbox.delete(0, "end")  # clears value
-        self.maxrollspinbox.insert(0, 200)  # sets value
-        self.maxrollspinbox.grid(row=4, column=0, sticky=tk.W)
+        self.max_roll_spinbox["validatecommand"] = (self.max_roll_spinbox.register(self.validate), '%P', '%i', '%d')
+        self.max_roll_spinbox.delete(0, "end")  # clears value
+        self.max_roll_spinbox.insert(0, 200)  # sets value
+        self.max_roll_spinbox.grid(row=4, column=0, sticky=tk.W)
 
         # Spinbox to set the custom unrolling position of the sunshade
-        customrollspinbox = tk.Spinbox(borderframe, from_=0, to=200, validate='all')
+        self.custom_roll_spinbox = tk.Spinbox(border_frame, from_=0, to=200, validate='all')
         # validates if int
-        customrollspinbox["validatecommand"] = (customrollspinbox.register(self.validate), '%P', '%i', '%d')
-        customrollspinbox.delete(0, "end")  # clears value
-        customrollspinbox.insert(0, 50)  # sets value
-        customrollspinbox.grid(row=8, column=0, sticky=tk.W)
+        self.custom_roll_spinbox["validatecommand"] = (self.custom_roll_spinbox.register(self.validate), '%P', '%i', '%d')
+        self.custom_roll_spinbox.delete(0, "end")  # clears value
+        self.custom_roll_spinbox.insert(0, 50)  # sets value
+        self.custom_roll_spinbox.grid(row=8, column=0, sticky=tk.W)
 
         # OK button applies the data and goes back to the mainpage
-        okbut = ttk.Button(borderframe, text="OK", width=10,
+        ok_button = ttk.Button(border_frame, text="OK", width=10,
                            command=lambda: controller.show_frame("Mainpage"))
-        okbut.grid(row=9, column=0, sticky=tk.SW)
+        ok_button.grid(row=9, column=0, sticky=tk.SW)
 
         # The Back button used to go back to the mainpage/frontpage
-        cancelbut = ttk.Button(borderframe, text="Cancel", width=10,
+        cancel_button = ttk.Button(border_frame, text="Cancel", width=10,
                                command=lambda: controller.show_frame("Mainpage"))
-        cancelbut.grid(row=9, column=0, sticky=tk.S)
+        cancel_button.grid(row=9, column=0, sticky=tk.S)
 
         # Applies the values that are set in the spinboxes and scale
         # custom unrolling position of the sunshade
-        applybut = ttk.Button(borderframe, text="apply", width=10)
-        applybut.bind("<Button-1>", self.getdata)
-        applybut.grid(row=9, column=0, sticky=tk.SE)
+        apply_button = ttk.Button(border_frame, text="apply", width=10)
+        apply_button.bind("<Button-1>", self.get_data)
+        apply_button.grid(row=9, column=0, sticky=tk.SE)
 
         # Button to edit the control unit name
-        editbut = ttk.Button(borderframe, text="edit")
-        editbut.bind("<Button-1>", self.editname)
-        editbut.grid(row=0, column=0, sticky=tk.E)
+        edit_button = ttk.Button(border_frame, text="edit")
+        edit_button.bind("<Button-1>", self.edit_name)
+        edit_button.grid(row=0, column=0, sticky=tk.E)
 
-        marginframe2 = ttk.Frame(self, padding=10)  # generates a frame within the frame with padding set to 10
-        marginframe2.grid(row=1, column=5)
+        margin_frame2 = ttk.Frame(self, padding=10)  # generates a frame within the frame with padding set to 10
+        margin_frame2.grid(row=1, column=5)
         # generates a labelframe within frame. padding set to 10
-        borderframe2 = tk.Frame(marginframe2, highlightbackground="grey", highlightthickness=1, padx=10, pady=10)
-        borderframe2.grid(row=0, column=5)
+        border_frame2 = tk.Frame(margin_frame2, highlightbackground="grey", highlightthickness=1, padx=10, pady=10)
+        border_frame2.grid(row=0, column=5)
 
-        marginframe2 = ttk.Frame(self, padding=10)  # generates a frame within the frame with padding set to 10
-        marginframe2.grid(row=1, column=5)
+        margin_frame2 = ttk.Frame(self, padding=10)  # generates a frame within the frame with padding set to 10
+        margin_frame2.grid(row=1, column=5)
         # generates a labelframe within frame. padding set to 10
-        borderframe2 = tk.Frame(marginframe2, highlightbackground="grey", highlightthickness=1, padx=10, pady=10)
-        borderframe2.grid(row=0, column=5)
+        border_frame2 = tk.Frame(margin_frame2, highlightbackground="grey", highlightthickness=1, padx=10, pady=10)
+        border_frame2.grid(row=0, column=5)
 
         # Shows label of text
-        graphinfo = tk.Label(borderframe2, text="Informatie Besturingseenheid")
-        graphinfo.grid(row=0, column=0, sticky=tk.W)
+        graph_info = tk.Label(border_frame2, text="Informatie Besturingseenheid")
+        graph_info.grid(row=0, column=0, sticky=tk.W)
 
-        empty_block = tk.Label(borderframe2)
+        empty_block = tk.Label(border_frame2)
         empty_block.grid(row=1, column=0, sticky=tk.W)
 
-        type_graph_text = tk.Label(borderframe2, text="Type Grafiek:")
+        type_graph_text = tk.Label(border_frame2, text="Type Grafiek:")
         type_graph_text.grid(row=2, column=0, sticky=tk.W)
 
-        graph2_text = tk.Label(borderframe2, text="Licht")
+        graph2_text = tk.Label(border_frame2, text="Licht")
         graph2_text.grid(row=3, column=0, sticky=tk.W)
 
-        empty_block2 = tk.Label(borderframe2)
+        empty_block2 = tk.Label(border_frame2)
         empty_block2.grid(row=4, column=0, sticky=tk.W)
 
-        x_as_label = tk.Label(borderframe2, text="X-as : Tijd in seconden")
+        x_as_label = tk.Label(border_frame2, text="X-as : Tijd in seconden")
         x_as_label.grid(row=5, column=0, sticky=tk.W)
 
-        y_as_label = tk.Label(borderframe2, text="Y-as : Lichtintensiteit in lx")
+        y_as_label = tk.Label(border_frame2, text="Y-as : Lichtintensiteit in lx")
         y_as_label.grid(row=6, column=0, sticky=tk.W)
 
-        empty_block3 = tk.Label(borderframe2)
+        empty_block3 = tk.Label(border_frame2)
         empty_block3.grid(row=7, column=0, sticky=tk.W)
 
         # Shows the label with the temperature
-        self.luxtext = tk.Label(borderframe2, text="Lichtintensiteit in lux : ")
-        self.luxtext.grid(row=8, column=0, sticky=tk.W)
+        self.lux_label = tk.Label(border_frame2, text="Lichtintensiteit in lux : ")
+        self.lux_label.grid(row=8, column=0, sticky=tk.W)
 
-        self.rollout_text = tk.Label(borderframe2, text="Uitgerold (cm): ")
-        self.rollout_text.grid(row=9, column=0, sticky=tk.W)
+        self.roll_out_text = tk.Label(border_frame2, text="Uitgerold (cm): ")
+        self.roll_out_text.grid(row=9, column=0, sticky=tk.W)
 
-        empty_block4 = tk.Label(borderframe2)
+        empty_block4 = tk.Label(border_frame2)
         empty_block4.grid(row=10, column=0, sticky=tk.W)
 
-        backbutton = ttk.Button(borderframe2, text="Terug naar de Hoofdpagina",
+        back_button = ttk.Button(border_frame2, text="Terug naar de Hoofdpagina",
                                 command=lambda: controller.show_frame("Mainpage"))
-        backbutton.grid(row=11, column=0, rowspan=4, sticky=tk.S)
+        back_button.grid(row=11, column=0, rowspan=4, sticky=tk.S)
 
-        self.updateGUI()
+        self.update_GUI()
 
     # validates if value is an integer
     def validate(self, instr, i, acttyp):  # validates if value is an integer
@@ -185,48 +178,48 @@ class ControlUnit(tk.Frame):
                 return False
         return True
 
-    def editname(self):
+    def edit_name(self):
         print("Not implemented yet")
 
-    def okbut(self):
+    def to_main_page(self):
         self.controller.show_frame("Mainpage")
 
-    def dataerror(self):
+    def data_error(self):
         messagebox.showwarning("Foutmelding",
                                "De minimale uitrolwaarde mag niet hoger zijn dan de maximale uitrolwaarde.")
 
-    def dataerror2(self):
+    def data_error2(self):
         messagebox.showwarning("Foutmelding",
                                "De handmatige uitrolwaarde valt niet binnen het minimum of maximum")
 
-    def dataerror3(self):
+    def data_error3(self):
         messagebox.showwarning("Foutmelding",
                                "Waarde te hoog!")
 
-    def getdata(self, a):
+    def get_data(self, a):
         # retrieves data from all spinboxes and converts them to ints
-        minroll = int(self.minrollspinbox.get())
-        maxroll = int(self.maxrollspinbox.get())
-        customroll = int(self.customrollspinbox.get())
+        min_roll = int(self.min_roll_spinbox.get())
+        max_roll = int(self.max_roll_spinbox.get())
+        custom_roll = int(self.custom_roll_spinbox.get())
         # the .get() function on a scale already retrieves an int
-        sensortrig = self.sensortrigscale.get()
+        sensortrig = self.sensor_trig_scale.get()
         #   create list of all retrieved data
-        datalist = [minroll, maxroll, customroll, sensortrig]
+        datalist = [min_roll, max_roll, custom_roll, sensortrig]
 
         # checks if maxroll is greater than minroll, otherwise throws an error
-        if minroll > maxroll:
+        if min_roll > max_roll:
             print("Throw an error")
-            self.dataerror()
+            self.data_error()
         # throws error if customroll is not within minimum and max
-        elif customroll > maxroll or customroll < minroll:
-            self.dataerror2()
+        elif custom_roll > max_roll or custom_roll < min_roll:
+            self.data_error2()
         # throws error when value is too high
-        elif minroll > 200 or maxroll > 200 or customroll > 200:
-            self.dataerror3()
+        elif min_roll > 200 or max_roll > 200 or custom_roll > 200:
+            self.data_error3()
         else:
             print(datalist)
 
-    def updateGUI(self):
+    def update_GUI(self):
         self.time = self.time + 1
 
         status = self.control_unit.request_status()
@@ -237,13 +230,13 @@ class ControlUnit(tk.Frame):
         self.a_Temp.clear()
         self.a_Temp.plot(self.x_Values, self.y_Values)
         self.a_Temp.set_ylim((0, 1000))
-        self.a_Temp.legend('hoi')
+
         canvas_temp = FigureCanvasTkAgg(self.fig_Temp, self)
         canvas_temp.show()
         canvas_temp.get_tk_widget().grid(row=0, column=2, rowspan=10)
 
-        self.luxtext.config(text="Lichtintensiteit in lux : " + str(status[1]))
-        self.rollout_text.config(text="Uitgerold (cm): " + str(status[0]))
+        self.lux_label.config(text="Lichtintensiteit in lux : " + str(status[1]))
+        self.roll_out_text.config(text="Uitgerold (cm): " + str(status[0]))
 
         self.update()
-        self.after(1000, self.updateGUI)
+        self.after(1000, self.update_GUI)
